f6159aff99eefac7a7df267088349510e8829ba2
ericofrs <ericoqv@gmail.com>
2025-04-25 16:10:30 +0200

    updated renv.lock

--- pipeline.R
+++ pipeline.R
@@ -1,4 +1,4 @@
-# library("pipelineR")
+# library(pipelineR)
 message("Starting Pipeline...")
 batch_size_jenkins <- as.integer(Sys.getenv("JENKINS_BATCH_SIZE"))
 message("Batch size: ", batch_size_jenkins)
